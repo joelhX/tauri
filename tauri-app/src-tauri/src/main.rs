04f6a6c9dfd8d529a44cf770cc6ef298f778475f
joelh <joelh@naver.com>
2023-03-13 23:08:42 +0900

    3453

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -6,7 +6,7 @@
 use screenshots::Screen;
 use std::{fs,time::Instant,process::Command};
 use tauri::SystemTray;
-
+use std::os::windows::process::CommandExt;
 //use captis::{init_capturer, Capturer};
 #[tauri::command]
 fn get_area(x:i32, y:i32, w:u32, h:u32) -> String {
@@ -14,29 +14,21 @@ fn get_area(x:i32, y:i32, w:u32, h:u32) -> String {
     let screens = Screen::all().unwrap();
     let image = screens[0].capture_area(x, y, w, h).unwrap();
     let buffer = image.buffer();
-    fs::write(format!("target/1-2.png" ), buffer).unwrap();
+    fs::write(format!("cap.png" ), buffer).unwrap();
     let elapsed = start.elapsed().as_millis();
     println!("{},{},{},{}",x, y, w, h);
 
     let out = Command::new("tesseract")
-    .args(["target/1-2.png","stdout","-l","kor+eng","--psm","4"])
+    .args(["cap.png","stdout","-l","kor+eng","--psm","4"])
+    .creation_flags(0x08000000)
+    .env(" user_defined_dpi", "128")
     .output()
     .expect("failed to execute process");
     let result=out.stdout;
-    println!("{}",String::from_utf8_lossy(&result));
-    format!("Hello, {}! You've been greeted from Rust!", elapsed)
+    //println!("{}",String::from_utf8_lossy(&result));
+    format!("elapsed {}\n ____text____\n{}", elapsed,String::from_utf8_lossy(&result))
 }
 
-/*fn greet(name: &str, x:i32, y:i32, w:u32, h:u32) -> String {
-    let start = Instant::now();
-    
-    let capturer = init_capturer().expect("Couldn't Initialize Capturer");
-    let image = capturer.capture(0).expect("Couldn't Capture Screen");
-    image.save("test.png").expect("Couldn't Save Image");
-    let elapsed = start.elapsed().as_millis();
-    format!("Hello, {}! You've been greeted from Rust!", elapsed)
-}
-*/
 
 
 fn main() {
